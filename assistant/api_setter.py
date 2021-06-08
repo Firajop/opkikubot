ffcd6889d6a677759c33acac7b466ddd310df4d9
opgohil <58036788+opgohil@users.noreply.github.com>
2021-06-09 02:23:21 +0530

    Update api_setter.py

--- assistant/api_setter.py
+++ assistant/api_setter.py
@@ -1,9 +1,9 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
+# opkikubot - UserBot
+# Copyright (C) 2020 opkikubot
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/opgohil/opkikubot/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://github.com/opgohil/opkikubot/blob/main/LICENSE/>.
 
 from . import *
 
