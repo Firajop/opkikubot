643fb76e757b6c962b000b054e59e103480d944a
opgohil <58036788+opgohil@users.noreply.github.com>
2021-06-09 02:46:19 +0530

    Update outgoing.py

--- assistant/Pmbot/outgoing.py
+++ assistant/Pmbot/outgoing.py
@@ -1,11 +1,11 @@
-# Ultroid - UserBot
-# Copyright (C) 2020 TeamUltroid
+# opkikubot - UserBot
+# Copyright (C) 2020 opkikubot
 #
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# This file is a part of < https://github.com/opgohil/opkikubot/ >
 # PLease read the GNU Affero General Public License in
-# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# <https://github.com/opgohil/opkikubot/blob/main/LICENSE/>.
 
-# https://github.com/xditya/TeleBot/blob/master/telebot/plugins/mybot/pmbot/outgoing.py
+
 
 from telethon import events
 
